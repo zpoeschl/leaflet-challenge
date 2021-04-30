655813d1243c09576b0c7507c7e0085d26956a2c
Zoe Poeschl <zpoeschl@gmail.com>
2021-04-30 13:04:34 -0500

    All markers and colors working. Just need legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,8 @@
 console.log("logic.js loaded");
 
 // API geojson endpoint
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+//var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
 // perform GET request to queryUrl
 d3.json(queryUrl).then(function(data) {
@@ -17,22 +18,29 @@ function createFeatures(earthquakeData) {
         layer.bindPopup("<h3>" + feature.properties.place + "</h3><p>" + feature.properties.mag + " magnitude</p><p>" + new Date(feature.properties.time) + "</p>");
     }
 
+    // set color range for markers
+    function getColor(d) {
+        return  d <= 10 ? "#00FF00" : //lime green
+                d >= 10 ? "#9acd32" : //yellow green
+                d >= 30 ? "#FFFF00" : //yellow
+                d >= 50 ? "f8d568" : //orange yellow
+                d >= 70 ? "FFA500" : //orange
+                d >= 90 ? "#FF0000" : //red
+                            "DC143C"; //crimson
+    }
+
     // create geojson layer containing features array
     // run onEachFeature once for each piece of data in the array
     var earthquakes = L.geoJSON(earthquakeData, {
         pointToLayer: function(feature, latlng) {
             return new L.circleMarker(latlng, {
                 radius: feature.properties.mag,
-                color: "white",
+                color: getColor(feature.geometry.coordinates[2]),
                 weight: 1,
-                fill: true
+                fill: true,
+                fillColor: getColor(feature.geometry.coordinates[2]),
+                fillOpacity: 1
             })
-        },
-        style: function(feature) {
-            return {
-                fillColor: feature.geometry.coordinates[2],
-                fillOpacity: feature.geometry.coordinates[2]
-            }
         },
         onEachFeature: onEachFeature
     });
