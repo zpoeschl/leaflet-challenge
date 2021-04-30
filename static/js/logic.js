0ee0a3219c8865d66b905b996456169c9b03b13d
Zoe Poeschl <zpoeschl@gmail.com>
2021-04-30 09:25:28 -0500

    Get earthquake data function created

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,2 +1,28 @@
 // API geojson endpoint
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// perform GET request to queryUrl
+d3.json(queryUrl).then(function(data) {
+    // send data.features object to createFeatures function
+    createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData) {
+
+    // function to run for each feature in features array
+    // give each feature a popup
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.place +
+            "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    }
+
+    // create geojson layer containing features array
+    // run onEachFeature once for each piece of data in the array
+    var earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature
+    });
+
+    // send earthquakes layer to createMap function
+    createMap(earthquakes);
+}
+
