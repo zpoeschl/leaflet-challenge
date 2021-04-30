5db2c0bfddf5e0b12bf5a8b02bea0db2e4ab8e5d
Zoe Poeschl <zpoeschl@gmail.com>
2021-04-30 10:04:49 -0500

    Corrected errors showing in console, however map still not showing

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,5 @@
+console.log("logic.js loaded");
+
 // API geojson endpoint
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
@@ -29,26 +31,16 @@ function createFeatures(earthquakeData) {
 function createMap(earthquakes) {
 
     // define map layer(s)
-    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-        tileSize: 512,
-        maxZoom: 18,
-        zoomOffset: -1,
-        id: "mapbox/streets-v11",
-        accessToken: API_KEY
-    });
-
-    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
-        id: "dark-v10",
+        id: "light-v10",
         accessToken: API_KEY
-    });
+      });
 
     // define baseMaps object to hold base layers
     var baseMaps = {
-        "Street Map": streetmap,
-        "Dark Map": darkmap
+        "Light Map": lightmap
     };
 
     // create overlap object to hold overlay earthquake layer
@@ -56,13 +48,13 @@ function createMap(earthquakes) {
         Earthquakes: earthquakes
     };
 
-    // create map + streetmap and earthquake layers to display on load
+    // create map + light map and earthquake layers to display on load
     var myMap = L.map("mapid", {
         center: [
             37.09, -95.71
         ],
         zoom: 5,
-        layers: [streetmap, earthquakes]
+        layers: [lightmap, earthquakes]
     });
 
     // create layer control
